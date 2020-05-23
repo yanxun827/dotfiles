77021f7ddd08422b6b9258f41074819e570d6451
Yan Xun Ng <>
2020-05-23 18:07:42 +0100

    Fix fzf default rg command to have correct syntax

--- .fzf.bash
+++ .fzf.bash
@@ -5,7 +5,7 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
 fi
 
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
