7ce7a60c4f8b9880ab8efd75a67d31b748747ee5
Yan Xun Ng <yanxun827@gmail.com>
2020-12-19 17:32:20 +0800

    Add brew.sh to the list of files to not be copied over by bootstrap, and remove unnecessary brew formulas

--- brew.sh
+++ brew.sh
@@ -2,6 +2,15 @@
 
 # Install command-line tools using Homebrew.
 
+function brew_install {
+  # Check if a brew formula is not installed, then installs it.
+  if ! brew ls --versions $1 > /dev/null; then
+    brew install $1
+  else
+  	echo "$1 is already installed"
+  fi
+}
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -11,20 +20,8 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+brew_install bash
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
@@ -32,70 +29,38 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install GNU core utilities (those that come with macOS are outdated).
+# `g`-prefixed.
+brew_install coreutils
+# Install some other useful utilities like `sponge`.
+brew_install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew_install findutils
+# Install GNU `sed`, overwriting the built-in `sed`, `g`-prefixed.
+brew_install gnu-sed
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# Install more recent GNU versions of some macOS tools.
+brew_install vim
+brew_install grep
+brew_install git
+brew_install php
+brew_install python
 
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+brew_install ack
+brew_install git-lfs
+brew_install tree
+brew_install wget 
+brew_install fzf
+# ${BREW_PREFIX}/opt/fzf/install
+
+# Create directory and store symlinks for all the gnu tools installed by homebrew. 
+# Don't forget to add the directory to `$PATH`.
+mkdir -p "$HOME/gnubin"
+echo "Creating symlinks for GNU binaries"
+for gnubinpath in /usr/local/opt/**/libexec/gnubin/*; do
+    ln -s $gnubinpath "$HOME/gnubin" &> /dev/null
+done
 
 # Remove outdated versions from the cellar.
 brew cleanup
